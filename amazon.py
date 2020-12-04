518ef4a8e481c29cab9a98aacc798a26b959944c
Julian <jchumtl@gmail.com>
2020-12-04 08:56:03 -0500

    Updating Amazon

--- amazon.py
+++ amazon.py
@@ -5,7 +5,7 @@ class ListNode:
         self.val = val
         self.next = next
 class Solution:
-    # Two Sum problem
+    #1 Two Sum problem 
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         numsSet = {}
         for index in range(len(nums)):
@@ -14,32 +14,6 @@ class Solution:
             difference = target - nums[index]
             if difference in numsSet and index is not numsSet[difference]:
                 return [index, numsSet[difference]]
-
-    # Longest Substring Without Repeating Characters
-    def lengthOfLongestSubstring(self, s: str) -> int:
-        start = 0
-        end = 0
-        endpoints = [0, 0]
-        maxLength = 0
-        letters = {}     
-        length = len(s)
-        
-        if length > 1:
-            while end < length:
-                char = s[end]
-                if char in letters:
-                    if start < letters[char]:
-                        start = letters[char] + 1
-
-                endpoints[0] = start
-                endpoints[1] = end
-                if (end - start) > maxLength:
-                    maxLength = endpoints[1] - endpoints[0]
-                letters[char] = end
-                end += 1
-        else: 
-            maxLength = length
-        return maxLength
 
     """
     Old IDs are palindromes
@@ -122,9 +96,50 @@ class Solution:
             fact *= i
         return fact
           
-s = Solution()
-# str = "au"
-# print(s.lengthOfLongestSubstring(str))
+    #3 Longest Substring Without Repeating Characters
+        # s = "ab"
+        # s = "abcabcbb"
+        # s = "pwwkew"
+        # s = " "
+        # s = "abba"
+        # s = "aab"
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        maxLength = 0
+        letters = {}
+        left = 0
+        right = 0
+        if len(s) > 1:
+            if len(s) == 2:
+                if s[0] == s[1]:
+                    maxLength = 1
+                else:
+                    maxLength = 2
+            else:
+                while right < len(s):
+                    letter = s[right]
+                    if (right - left) > maxLength:
+                        maxLength = right - left
+                    if letter in letters:
+                        left = letters[letter] + 1
+                    letters[letter] = right
+                    right += 1
+        else: 
+            maxLength = len(s)
+        return maxLength
+
+    #11 Container With Most Water
+    def maxArea(self, height: List[int]) -> int:
+        possibleCombinations = int(factorial(numRobots) / factorial (2) / factorial(numRobots - 2))
+        
+        count = 0
+        left = 0
+        right = 1
+
+        volume = min(n[right], n[left]) * (right - left)
+ 
+        # while count < possibleCombinations:
+            
 
 
 
+solution = Solution()
